d7ef826f4d18265edf5859c9c91bc0653eaf7a43
wcarpenter98 <50806560+wcarpenter98@users.noreply.github.com>
2021-03-10 03:30:02 -0500

    testing resources

---
+++ config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_qP9GsxOJB', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-2', // e.g. us-east-2
+        clientId: '8ttgl6soe8m5plm5kg40uv6c1' //is this used anywhere?
+    },
+};
