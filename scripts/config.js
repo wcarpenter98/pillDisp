0ecf0b3ef1ae9be4fec982812d0e0f6df604a447
wcarpenter98 <50806560+wcarpenter98@users.noreply.github.com>
2021-03-10 04:54:20 -0500

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_qP9GsxOJB', // e.g. us-east-2_uXboG5pAb
+        userPoolId: ' us-east-2_o5ZNByktg', // e.g. us-east-2_uXboG5pAb
         region: 'us-east-2', // e.g. us-east-2
-        clientId: '8ttgl6soe8m5plm5kg40uv6c1' //is this used anywhere?
-    },
+        clientId: '5keoibnkqbl25e550k3apve09' //is this used anywhere?
+    }, const pool_region = 'us-east-2';
 };
