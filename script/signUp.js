46b37d6afc2f62658f88b38cfa4b4d20e1d03732
wcarpenter98 <50806560+wcarpenter98@users.noreply.github.com>
2021-03-10 02:43:31 -0500

    Update signUp.js

--- script/signUp.js
+++ script/signUp.js
@@ -1,25 +1,22 @@
+import { Auth } from 'aws-amplify';
 
-
-
-    import {Auth} from 'aws-amplify';
-
-          async function signUp() {
-              try {
+async function signUp() {
+    try {
         phone_number = "3048162450";
-                  password = "password";
-                  username = "username";
-                  email = "wcarpenter982@gmail.com";
-                  const {user} = await Auth.signUp({
-        username,
-        password,
-        attributes: {
-        email,          // optional
-        phone_number,   // optional - E.164 number convention
-                          // other custom attributes 
-                      }
-                  });
-                  console.log(user);
-              } catch (error) {
+        password = "password";
+        username = "username";
+        email = "wcarpenter982@gmail.com";
+        const { user } = await Auth.signUp({
+            username,
+            password,
+            attributes: {
+                email,          // optional
+                phone_number,   // optional - E.164 number convention
+                // other custom attributes 
+            }
+        });
+        console.log(user);
+    } catch (error) {
         console.log('error signing up:', error);
-              }
-          }
+    }
+}
